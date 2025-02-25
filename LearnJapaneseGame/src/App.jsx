69cac940785c28c2d62ae72fe3c02bc69328cff4
Nicolas Steger <njsteger@hawaii.edu>
2025-02-24 18:31:46 -1000

    changes

--- LearnJapaneseGame/src/App.jsx
+++ LearnJapaneseGame/src/App.jsx
@@ -1,23 +1,23 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
-import Home from './pages/Home.jsx';
+import Home from './pages/Home';
 import Game from './pages/Game';
 import About from './pages/About';
+import Navbar from './components/Navbar';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <Router>
       <main>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game" element={<Game />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
-      <Footer />
     </Router>
   );
 };
--- LearnJapaneseGame/src/components/Navbar.jsx
+++ LearnJapaneseGame/src/components/Navbar.jsx
@@ -1,17 +1,16 @@
 // src/components/Navbar.jsx
 import React from 'react';
-import { Link } from 'react-router-dom'; // for internal navigation
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/game">Game</Link></li>
-        <li><Link to="/about">About</Link></li>
+    <nav className="bg-blue-600 text-white p-4 shadow-md">
+      <ul className="flex justify-around">
+        <li><a href="/" className="hover:text-gray-300">Home</a></li>
+        <li><a href="/about" className="hover:text-gray-300">About</a></li>
+        <li><a href="/contact" className="hover:text-gray-300">Contact</a></li>
       </ul>
     </nav>
   );
-};
+}
 
 export default Navbar;
